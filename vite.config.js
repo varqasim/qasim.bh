a4e461230967541b0d2af99aac0171b4e729f8ec
qasim <qasim@calo.app>
2025-06-07 02:42:52 +0300

    fix: base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/qasim.bh',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
